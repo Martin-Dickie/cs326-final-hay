3ad2d5905ad5b3cfc32cb328bd6350a8c3ef02c9
Anttl462 <aarinaldi@users.noreply.github.com>
2020-11-09 17:54:44 -0600

    Better DOM surgery, added remove friend function

--- lobbies.js
+++ lobbies.js
@@ -3,10 +3,10 @@
 const url = "https://somethingsomethingsomething.herokuapp.com";
 
 const user = 'test'; // 
-getFriendInfo();
-getLobbyInfo();
+getAndRenderFriendInfo();
+getAndRenderLobbyInfo();
 
-async function getFriendInfo() {
+async function getAndRenderFriendInfo() {
     const userInfoResponse = await fetch(url + '/readUser?name='+user);
     if (userInfoResponse.ok) { 
         const userInfo = await userInfoResponse.json();
@@ -17,7 +17,34 @@ async function getFriendInfo() {
 
                 // Add friends to page
                 const tableBody = document.getElementById("friend-table-body");
-                tableBody.innerHTML += '<tr> <th scope=\"row\">'+ friendsInfo.name +'</th> <td>'+ friendsInfo.status +'</td></tr>';
+                const newRow = document.createElement('tr');
+                newRow.setAttribute('id', `${i}`);
+                const idElement = document.createElement('th');
+                idElement.setAttribute('scope', 'row');
+                idElement.innerText = friendsInfo.name;
+                const nameElement = document.createElement('tr');
+                nameElement.innerText = friendsInfo.name;
+                const statusElement = document.createElement('td');
+                statusElement.innerText = friendsInfo.status;
+                newRow.appendChild(nameElement);
+                newRow.appendChild(statusElement);
+
+                // Action Listener for Removing Friend
+                newRow.addEventListener('click', function() {
+                    if (confirm('Remove Friend?')) {
+                            // Remove Friend
+                            const updatedUser = JSON.parse(JSON.stringify(userInfo));
+                            updatedUser.friends = userInfo.friends.splice(parseInt(newRow.id), 1);
+                            fetch(url + "/updateUser", { 
+                                method: "POST", 
+                                body: JSON.stringify(updatedUser), 
+                                headers: { 
+                                    "Content-type": "application/json; charset=UTF-8"
+                                } 
+                        });
+                        }
+                });
+                tableBody.appendChild(newRow);
             } else {
                 alert('HTTP-Error: ' + friendsInfosResponse.status);
             }
@@ -38,18 +65,33 @@ async function getFriendInfo() {
         users,
     }
 */
-async function getLobbyInfo() {
+async function getAndRenderLobbyInfo() {
     const allLobbyInfoResponse = await fetch(url + '/readAllLobbies');
     if (allLobbyInfoResponse.ok) { 
         const allLobbyInfo = await allLobbyInfoResponse.json();
         for(let i = 0; i < allLobbyInfo.length; i++) {
             // Add lobby to page
-            const tableBody = document.getElementById("lobby-browser-table");
-            tableBody.innerHTML += '<tr> <th scope="row">'+i+'</th><td>'+
-                                    allLobbyInfo[i].name +'</td><td>'+ 
-                                    allLobbyInfo[i].game +'</td> <td>'+ 
-                                    allLobbyInfo[i].status +'</td> <td>'+ 
-                                    allLobbyInfo.users+'/'+ allLobbyInfo.maxplayers +'</td></tr>';
+            const tableBody = document.getElementById("friend-table-body");
+            const newRow = document.createElement('tr');
+            const idElement = document.createElement('th');
+            idElement.setAttribute('scope', 'row');
+            idElement.innerText = i;
+            const nameElement = document.createElement('td');
+            nameElement.innerText = allLobbyInfo[i].name;
+            const gameElement = document.createElement('td');
+            gameElement.innerText = allLobbyInfo[i].game;
+            const statusElement = document.createElement('td');
+            statusElement.innerText = allLobbyInfo[i].status;
+            const sizeElement = document.createElement('td');
+            sizeElement.innerText = allLobbyInfo[i].size+'/'+allLobbyInfo[i].maxplayers;
+
+            newRow.appendChild(idElement);
+            newRow.appendChild(nameElement);
+            newRow.appendChild(gameElement);
+            newRow.appendChild(statusElement);
+            newRow.appendChild(sizeElement);
+
+            tableBody.appendChild(newRow);
         }
     } else {
         alert('HTTP-Error: ' + allLobbyInfoResponse.status);
