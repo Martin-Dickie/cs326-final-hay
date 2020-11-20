770e0a6350db7e5d6d0bfb5f31517c029915a4b8
Anttl462 <aarinaldi@users.noreply.github.com>
2020-11-19 19:02:43 -0600

    Fixed procfile?

--- lobbies.js
+++ lobbies.js
@@ -3,20 +3,22 @@
 const url = "https://somethingsomethingsomething.herokuapp.com";
 
 const user = 'test'; // 
-getAndRenderFriendInfo();
-getAndRenderLobbyInfo();
+getAndRenderFriendInfo(document.getElementById('friend-table-body'));
+getAndRenderLobbyInfo(document.getElementById('lobby-browser-table'));
 
-async function getAndRenderFriendInfo() {
+async function getAndRenderFriendInfo(element) {
+    while (element.firstChild) {
+        element.firstChild.remove();
+    }
     const userInfoResponse = await fetch(url + '/readUser?name='+user);
     if (userInfoResponse.ok) { 
         const userInfo = await userInfoResponse.json();
-        for(const friendName of userInfo.friends) {
-            const friendsInfosResponse = await fetch(url + '/readUser?name='+friendName);
+        for(let i = 0; i < userInfo.friends.length; i++) {
+            const friendsInfosResponse = await fetch(url + '/readUser?name='+userInfo.friends[i].name);
             if (friendsInfosResponse.ok) { 
                 const friendsInfo = await friendsInfosResponse.json();
 
                 // Add friends to page
-                const tableBody = document.getElementById("friend-table-body");
                 const newRow = document.createElement('tr');
                 newRow.setAttribute('id', `${i}`);
                 const idElement = document.createElement('th');
@@ -32,19 +34,21 @@ async function getAndRenderFriendInfo() {
                 // Action Listener for Removing Friend
                 newRow.addEventListener('click', function() {
                     if (confirm('Remove Friend?')) {
-                            // Remove Friend
-                            const updatedUser = JSON.parse(JSON.stringify(userInfo));
-                            updatedUser.friends = userInfo.friends.splice(parseInt(newRow.id), 1);
-                            fetch(url + "/updateUser", { 
-                                method: "POST", 
-                                body: JSON.stringify(updatedUser), 
-                                headers: { 
-                                    "Content-type": "application/json; charset=UTF-8"
-                                } 
+                        // Remove Friend
+                        const updatedUser = JSON.parse(JSON.stringify(userInfo));
+                        updatedUser.friends = userInfo.friends.splice(parseInt(newRow.id), 1);
+                        fetch(url + "/updateUser", {
+                            method: "POST",
+                            body: JSON.stringify(updatedUser), 
+                            headers: { 
+                                "Content-type": "application/json; charset=UTF-8"
+                            } 
                         });
-                        }
+                        // Re-render friends list:
+                        getAndRenderFriendInfo();
+                    }
                 });
-                tableBody.appendChild(newRow);
+                element.appendChild(newRow);
             } else {
                 alert('HTTP-Error: ' + friendsInfosResponse.status);
             }
@@ -65,13 +69,15 @@ async function getAndRenderFriendInfo() {
         users,
     }
 */
-async function getAndRenderLobbyInfo() {
+async function getAndRenderLobbyInfo(element) {
+    while (element.firstChild) {
+        element.firstChild.remove();
+    }
     const allLobbyInfoResponse = await fetch(url + '/readAllLobbies');
     if (allLobbyInfoResponse.ok) { 
         const allLobbyInfo = await allLobbyInfoResponse.json();
         for(let i = 0; i < allLobbyInfo.length; i++) {
             // Add lobby to page
-            const tableBody = document.getElementById("friend-table-body");
             const newRow = document.createElement('tr');
             const idElement = document.createElement('th');
             idElement.setAttribute('scope', 'row');
@@ -91,7 +97,7 @@ async function getAndRenderLobbyInfo() {
             newRow.appendChild(statusElement);
             newRow.appendChild(sizeElement);
 
-            tableBody.appendChild(newRow);
+            element.appendChild(newRow);
         }
     } else {
         alert('HTTP-Error: ' + allLobbyInfoResponse.status);
