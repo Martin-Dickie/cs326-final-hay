05806a41ee04ef5f6b5de0f352f2a1160b8465fd
martin-dickie <mbdickie@umass.edu>
2020-11-22 23:47:00 -0500

    Made games.js, mostly got the API working, but it's weird because of steam API not allowing for client tests due to CORS

---
+++ games.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { ObjectId } = require("mongodb");
+
+const url = "https://floating-plateau-01072.herokuapp.com";
+
+const secrets = require('./secrets.json');
+
+
+const steamKey = secrets.steamKey;
+const steamAPIURL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/key=" + steamKey;
+
+const games = [];
+
+async function makegameDB() {
+    const response = await fetch('./steamapps.json');
+    let gameList;
+    if (response.ok) {
+        gameList = await response.json();
+        gameList = gameList.applist.apps;
+    }
+    else {
+        console.error("Failed to read steamapps.json failed to load");
+    }
+    
+    for (const apps of gameList){
+        const game = await fetch("https://store.steampowered.com/api/appdetails?appids=" + apps.appid);
+        if(game.ok){
+            let gameApp = await game.json();
+            gameApp.categories.forEach( category => { if(category["description"] === "multiplayer") { games.add(gameApp); } } );
+        }
+
+    }
+}
